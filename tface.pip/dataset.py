26e4cc827aaf09517a9ee03f4828fa19469c3d79
Peter La Valle <peter.lavalle@gmail.com>
2024-06-24 19:14:51 +0100

    getting ready to skip annotations that're alreay present

--- tface.pip/dataset.py
+++ tface.pip/dataset.py
@@ -5,6 +5,7 @@ def ensure_directory_exists(file_path):
 	directory = os.path.dirname(file_path)
 	if not os.path.exists(directory):
 		os.makedirs(directory)
+	return directory
 def download_file(url, save_path):
 	
 	ensure_directory_exists(save_path)
@@ -52,60 +53,100 @@ def safe_write(path):
 
 import zipfile
 import os
+import cv2
 
+def throw(message):
+	print('\n')
+	raise Exception(f"{message}\n\n")
+
+# the validation set
+root = 'target/dataset/validation/'
+labels = 'wider_face_split/wider_face_val_bbx_gt.txt'
 
 # extract the images
+with zipfile.ZipFile('target/wider.validation.zip', 'r') as full:
+	for file_info in full.infolist():
+		if file_info.filename.endswith('.jpg'):
+			name = file_info.filename 
+			name = root+name[name.find('images/'):]
+			if not os.path.isfile(name):
+				ensure_directory_exists(name)
+				with open(name, 'wb') as file:
+					file.write(full.read(file_info))
 
 # extract the labels
-with zipfile.ZipFile('target/wider.annotations.zip', 'r').open('wider_face_split/wider_face_val_bbx_gt.txt') as file_in_zip:
-	jpeg = 'target/dataset/validation/images/'
-	into = 'target/dataset/validation/annotations/'
-	data = iter(file_in_zip.read().decode('utf-8').splitlines())
-	def line():
-		return next(data)
+with zipfile.ZipFile('target/wider.annotations.zip', 'r').open(labels) as file_in_zip:
+	
+	##
+	# finky recreation of an iterator
+	data = {
+		'next': 0,
+		'full': file_in_zip.read().decode('utf-8').splitlines(),
+		'more': True
+	}
+	
+	def line(data):
+		item = data['full'][data['next']]
+		data['next'] += 1
+		data['more'] = data['next'] < len(data['full'])
+		return item
+
+	
 	print('')
-	while data:
-		try:
-			name = line()
-			count = int(line())
-			print(f"==== {name}")
-			print(f"has {count} faces")
 
-			if not name.endswith('.jpg'):
-				raise Exception(f"the file `{name}` does not end with .jpg")
+	
+	def xml_write(xml, string):
+		xml.write(string + '\n')
 
-			print('TODO; get height')
-			width = ' ?width? '
-			height = ' ?height? '
+	while data['more']:
 
-			with safe_write(into + name[:-3] + 'xml') as xml:
-				xml.write(f'<annotation>\n')
-				xml.write(f'\t<filename>{name}</filename>\n')
-				xml.write(f'\t<size>\n')
-				xml.write(f'\t\t<width>?width?</width>\n')
-				xml.write(f'\t\t<height>?height?</height>\n')
-				xml.write(f'\t</size>\n')
+		# read the file name for this entry
+		name = line(data)
+		if not name.endswith('.jpg'):
+			raise Exception(f"the file `{name}` does not end with .jpg")
 
-				for i in range(count):
-					text = line().split(' ')
-					x, y, w, h, *_ = text
+		# open teh label xml file
+		xml = root+ 'annotations/' +name[:-3] + 'xml'
+		xml = safe_write(xml)
 
+		# load the count of targets
+		count = int(line(data))
 
-					xml.write(f'\t<object>\n')
-					xml.write(f'\t\t<name>face</name>\n')
-					xml.write(f'\t\t<bndbox>\n')
-					xml.write(f'\t\t\t<xmin>100</xmin>\n')
-					xml.write(f'\t\t\t<ymin>120</ymin>\n')
-					xml.write(f'\t\t\t<xmax>250</xmax>\n')
-					xml.write(f'\t\t\t<ymax>350</ymax>\n')
-					xml.write(f'\t\t</bndbox>\n')
-					xml.write(f'\t</object>\n')
+		# load the image and find its dimensions
+		image = root+'images/'+name
+		if not os.path.isfile(image):
+			print('\n')
+			raise Exception(f"the file `{image}` does not exist\n\n")
+		image = cv2.imread(image)
+		if image is None:
+			throw(f'failed to load {name}')
+		height, width, channels = image.shape
 
-				xml.write(f'</annotation>\n')
-			print('')
-		except StopIteration:
-			data = None
-			break
+		# write the header for the record
+		xml_write(xml, f'<annotation>')
+		xml_write(xml, f'\t<filename>{name}</filename>')
+		xml_write(xml, f'\t<size>')
+		xml_write(xml, f'\t\t<width>{width}</width>')
+		xml_write(xml, f'\t\t<height>{height}</height>')
+		xml_write(xml, f'\t</size>')
+
+		# wander through each item in the record
+		for i in range(count):
+			text = line(data).split(' ')
+			x, y, w, h, *_ = text
+
+			xml_write(xml, f'\t<object>')
+			xml_write(xml, f'\t\t<name>face</name>')
+			xml_write(xml, f'\t\t<bndbox>')
+			xml_write(xml, f'\t\t\t<xmin>{x}</xmin>')
+			xml_write(xml, f'\t\t\t<ymin>{y}</ymin>')
+			xml_write(xml, f'\t\t\t<xmax>{x+w}</xmax>')
+			xml_write(xml, f'\t\t\t<ymax>{y+h}</ymax>')
+			xml_write(xml, f'\t\t</bndbox>')
+			xml_write(xml, f'\t</object>')
+
+		xml_write(xml, f'</annotation>')
+		xml.close()
 
 
 raise Exception('do the training set')
@@ -116,21 +157,21 @@ print("data set ready - okie dokee")
 
 
 # <annotation>
-#     <filename>image1.jpg</filename>
-#     <size>
-#         <width>640</width>
-#         <height>480</height>
-#     </size>
-#     <object>
-#         <name>person</name>
-#         <bndbox>
-#             <xmin>100</xmin>
-#             <ymin>120</ymin>
-#             <xmax>250</xmax>
-#             <ymax>350</ymax>
-#         </bndbox>
-#     </object>
-#     <!-- Additional objects if present -->
+#	 <filename>image1.jpg</filename>
+#	 <size>
+#		 <width>640</width>
+#		 <height>480</height>
+#	 </size>
+#	 <object>
+#		 <name>person</name>
+#		 <bndbox>
+#			 <xmin>100</xmin>
+#			 <ymin>120</ymin>
+#			 <xmax>250</xmax>
+#			 <ymax>350</ymax>
+#		 </bndbox>
+#	 </object>
+#	 <!-- Additional objects if present -->
 # </annotation>
 
 
