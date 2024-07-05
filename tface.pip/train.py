27e7af5947c403ed0a974a75d4dc30ba0a70cb6c
Peter La Valle <peter.lavalle@gmail.com>
2024-07-05 16:45:04 +0100

    externalised the ANN creation
    --HG--
    branch : normalise-images

--- tface.pip/train.py
+++ tface.pip/train.py
@@ -27,36 +27,11 @@ import os
 import numpy as np
 from tensorflow.keras.layers.experimental.preprocessing import Resizing
 
-# Define the model for 1920x1080 input and 192x108 output
-def simple_cnn_model():
-	inputs = Input(input_size)
-
-	# resize the first one
-
-	# peter; is thia bsd?
-	resized = Conv2D(4, 3, activation='relu', padding='same')(inputs)
-	
-
-	
-	# Example simple CNN architecture
-	conv1 = Conv2D(64, 3, activation='relu', padding='same')(resized)
-	conv2 = Conv2D(64, 3, activation='relu', padding='same')(conv1)
-	pool1 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(conv2)
-	
-	conv3 = Conv2D(128, 3, activation='relu', padding='same')(pool1)
-	conv4 = Conv2D(128, 3, activation='relu', padding='same')(conv3)
-	pool2 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(conv4)
-	
-	conv5 = Conv2DTranspose(64, 3, strides=(2, 2), activation='relu', padding='same')(pool2)
-	conv6 = Conv2DTranspose(32, 3, strides=(2, 2), activation='relu', padding='same')(conv5)
-	
-	outputs = Conv2D(1, 1, activation='sigmoid', padding='same')(conv6)  # Adjust activation and padding as needed
-	
-	model = Model(inputs=inputs, outputs=outputs)
-	return model
-
 # Define the model
-model = simple_cnn_model()
+import network
+model = network.main(
+	input_size,
+)
 
 # Compile the model
 model.compile(optimizer=Adam(learning_rate=0.001), loss='binary_crossentropy', metrics=['accuracy'])
