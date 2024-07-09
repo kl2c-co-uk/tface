f67b09c5049648e7419a01d5166ca996839fcacf
Peter La Valle <peter.lavalle@gmail.com>
2024-07-09 17:43:53 +0100

    potential progress (need to turnt he list of boxes into heat-map)
    --HG--
    branch : segment

--- tface.pip/preview.py
+++ tface.pip/preview.py
@@ -7,29 +7,112 @@ import matplotlib.pyplot as plt
 from tensorflow.keras.layers import Input, Conv2D, UpSampling2D
 
 from tensorflow.keras.layers import Input, Resizing
+import tensorflow as tf
+import tensorflow_hub as hub
+from tensorflow.keras.layers import Input, Lambda
+from tensorflow.keras.models import Model
 
 import dataset
 
+def modelmake_model(input_shape):
+	import tensorflow as tf
+	import tensorflow_hub as hub
+	from tensorflow.keras.layers import Input, Lambda
+	from tensorflow.keras.models import Model
+
+	# Load the pre-trained face detection model from TensorFlow Hub
+	face_detector = hub.load("https://tfhub.dev/tensorflow/ssd_mobilenet_v2/2")
+	detector_output = face_detector(images)
+	return detector_output['detection_boxes']
+
+	def detect_faces(images):
+		return face_detector(images)['detection_boxes']
+
+	# Assuming input_shape is defined somewhere before
+	input_image = Input(shape=input_shape)
+
+	# Resize the input image to a desired size, e.g., (224, 224)
+	resized_image = tf.keras.layers.Resizing(height=224, width=224)(input_image)
+
+	# Convert resized image to grayscale
+	grayscale_image = tf.image.rgb_to_grayscale(resized_image)
+
+	# Detect faces in the image
+	
+
+	# Combine grayscale image with face detection boxes for visualization or further processing
+	combined_output = Lambda(lambda x: (x[0], x[1]))([grayscale_image, faces])
+
+	# Define the model
+	model = Model(inputs=input_image, outputs=combined_output)
+
+	# Model summary
+	model.summary()
+	return model
 
 def main():
 	input_shape = (1080, 1920, 3)
 
-	##
-	# build a model
+	# ##
+	# # build a model
 
 	input_image = Input(shape=input_shape)
 
 	# bottom/start of the network is just ... a 1080p RGB image
 	model = input_image
+	
+	print(f'''\n>>>input
+	{model}
+	''')
 
 	# convert it to greyscale
-	model = RGBToGrayscaleLayer()(model)
+	# model = tf.image.rgb_to_grayscale(model)
+	# print(f'''>>>	greyscale
+	# {model}
+	# ''')
 
-	model = Resizing(height=224, width=224)(model)
+
+	# resize
+	model = tf.keras.layers.Resizing(height=224, width=224)(model)
+
+
+	print(f'''\n>>>resized
+	{model}
+	''')
+
+
+
+	# Load the pre-trained face detection model from TensorFlow Hub
+	face_detector = hub.load("https://tfhub.dev/tensorflow/ssd_mobilenet_v2/2")
+	def lamdba(images):
+		
+		# squish into a uint8 layer
+		images = tf.clip_by_value(images, 0, 1)
+		images *= 255.0
+		images = tf.cast(images, tf.uint8)
+		
+		# do the detection
+		images = face_detector(images)
+		images = images['detection_boxes']
+		return images
+	model = Lambda(lamdba)(model)
+
+
+	print(f'''\n>>>face_detector
+	{model}
+	''')
+
+
+	raise Exception(
+		'so i think we now have 1x100x4 tensor with the deteacted faces in some form coming out of it'
+	)
+
 
 
 	model = Model(inputs=input_image, outputs=model)
 
+
+	# raise '???'
 
 	##
 	# show some junk about the model
@@ -98,22 +181,6 @@ def main():
 
 	plt.show()
 
-# Custom layer to convert RGB image to grayscale using specified weights
-class RGBToGrayscaleLayer(Layer):
-    def __init__(self, **kwargs):
-        super(RGBToGrayscaleLayer, self).__init__(**kwargs)
-
-    def call(self, inputs):
-        # Extract RGB channels
-        r, g, b = inputs[..., 0], inputs[..., 1], inputs[..., 2]
-
-        # Convert to grayscale using specified weights
-        gray = 0.299 * r + 0.587 * g + 0.114 * b
-        
-        # Expand dimensions to make it (batch_size, height, width, 1)
-        gray = tf.expand_dims(gray, axis=-1)
-        
-        return gray
 
 
 if __name__ == '__main__':
