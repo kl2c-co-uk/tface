a5ec31daef56cb105f20a5c68da9b4de5dfa5a17
Peter LaValle <peter.lavalle@gmail.com>
2024-08-06 19:44:26 +0100

    this seems to indicate that (1) eitehr the network is crazy becasue i trained it wrong or (2) i'm note decoding the results wrong
    --HG--
    branch : refactor

--- workspace-tface.unity/Assets/Scenes/TensorScript.cs
+++ workspace-tface.unity/Assets/Scenes/TensorScript.cs
@@ -11,69 +11,68 @@ using kl2c;
 
 public class TensorScript : MonoBehaviour
 {
-    public NNModel modelAsset;
+	public NNModel modelAsset;
 
-    public SpinCube cube1;
-    public SpinCube cube2;
-    void Start()
-    {
-        if (modelAsset == null)
-        {
-            Debug.LogError("Model asset not set in the Inspector");
-            return;
-        }
+	public SpinCube cube1;
+	public SpinCube cube2;
+	void Start()
+	{
+		if (modelAsset == null)
+		{
+			Debug.LogError("Model asset not set in the Inspector");
+			return;
+		}
 
-        yoloPipe = new kl2c.YoloPipe(modelAsset);
+		yoloPipe = new kl2c.YoloPipe(modelAsset);
 
-        var height = yoloPipe.runtimeModel.inputs[0].shape[5];
-        var width = yoloPipe.runtimeModel.inputs[0].shape[6];
+		var (width, height) = yoloPipe.Size;
 
-        inputTesorRenderTexture = new RenderTexture(width, height, 0);
-    }
+		inputTesorRenderTexture = new RenderTexture(width, height, 0);
+	}
 
-    public WebcamDisplay webcamDisplay;
-    private RenderTexture inputTesorRenderTexture;
-    public Texture2D outputTexture2D;
-
-
-    [UnityEngine.Range(0, 1)]
-    public float DetectionThreshold = 0.3f;
-    [UnityEngine.Range(0, 1)]
-    public float NmsThreshold = 0.5f;
-    [UnityEngine.Range(0, 1)]
-    public float ConfidenceThreshold = 0.4f;
-
-    public Material outputMaterial;
-    private kl2c.YoloPipe yoloPipe;
-
-    void Update()
-    {
-        var webcamTexture = webcamDisplay.webcamTexture;
-        // Check if the webcam has provided new data
-        if (!webcamTexture.didUpdateThisFrame)
-        {
-            return;
-        }
+	public WebcamDisplay webcamDisplay;
+	private RenderTexture inputTesorRenderTexture;
+	public Texture2D outputTexture2D;
 
 
-        // Convert the webcam texture to a Tensor
-        Graphics.Blit(source: webcamTexture, dest: inputTesorRenderTexture);
+	[UnityEngine.Range(0, 1)]
+	public float DetectionThreshold = 0.3f;
+	[UnityEngine.Range(0, 1)]
+	public float NmsThreshold = 0.5f;
+	[UnityEngine.Range(0, 1)]
+	public float ConfidenceThreshold = 0.4f;
 
-        var detectionResults = yoloPipe.Execute(inputTesorRenderTexture, DetectionThreshold, NmsThreshold, ConfidenceThreshold);
+	public Material outputMaterial;
+	private kl2c.YoloPipe yoloPipe;
 
-        // create thge output texture if we need to
-        if (null == outputTexture2D)
-            outputMaterial.mainTexture = outputTexture2D = outputTexture2D = new Texture2D(inputTesorRenderTexture.width, inputTesorRenderTexture.height);
+	void Update()
+	{
+		var webcamTexture = webcamDisplay.webcamTexture;
+		// Check if the webcam has provided new data
+		if (!webcamTexture.didUpdateThisFrame)
+		{
+			return;
+		}
 
-        // fill it randomly
-        outputTexture2D.Confetti(detectionResults);
 
-        // push the changes to the GPU
-        outputTexture2D.Apply();
-    }
+		// Convert the webcam texture to a Tensor
+		Graphics.Blit(source: webcamTexture, dest: inputTesorRenderTexture);
 
-    private void OnDestroy()
-    {
-        yoloPipe?.Dispose();
-    }
+		var detectionResults = yoloPipe.Execute(inputTesorRenderTexture, DetectionThreshold, NmsThreshold, ConfidenceThreshold);
+
+		// create thge output texture if we need to
+		if (null == outputTexture2D)
+			outputMaterial.mainTexture = outputTexture2D = outputTexture2D = new Texture2D(inputTesorRenderTexture.width, inputTesorRenderTexture.height);
+
+		// fill it randomly
+		outputTexture2D.Confetti(detectionResults);
+
+		// push the changes to the GPU
+		outputTexture2D.Apply();
+	}
+
+	private void OnDestroy()
+	{
+		yoloPipe?.Dispose();
+	}
 }
--- workspace-tface.unity/Assets/Scenes/asiryan/FaceChopped.cs
+++ workspace-tface.unity/Assets/Scenes/asiryan/FaceChopped.cs
@@ -10,185 +10,185 @@ using System.Linq;
 public static class FaceChopped
 {
 
-    public static List<Rect> DekkuTree(Tensor outTensor)
-    {
-        List<Rect> boxes = new List<Rect>();
-        List<int> indices = new List<int>();
-        List<float> scores = new List<float>();
+	public static List<Rect> DekkuTree(Tensor outTensor)
+	{
+		List<Rect> boxes = new List<Rect>();
+		List<int> indices = new List<int>();
+		List<float> scores = new List<float>();
 
-        int numRows = outTensor.shape[0]; // number of rows
-        int numCols = outTensor.shape[1]; // number of columns
+		int numRows = outTensor.shape[0]; // number of rows
+		int numCols = outTensor.shape[1]; // number of columns
 
-        Debug.Log(
-            "numCols = " + numCols
-            + "\n\tnumRows = " + numRows  );
-        for (int r = 0; r < numRows; r++)
-        {
-            float cx = outTensor[r, 0];
-            float cy = outTensor[r, 1];
-            float w = outTensor[r, 2];
-            float h = outTensor[r, 3];
-            float sc = outTensor[r, 4]; // sc = score? is this the likelyhood that SOMETHING is there?
+		Debug.Log(
+			"numCols = " + numCols
+			+ "\n\tnumRows = " + numRows  );
+		for (int r = 0; r < numRows; r++)
+		{
+			float cx = outTensor[r, 0];
+			float cy = outTensor[r, 1];
+			float w = outTensor[r, 2];
+			float h = outTensor[r, 3];
+			float sc = outTensor[r, 4]; // sc = score? is this the likelyhood that SOMETHING is there?
 
-            // Retrieve the confidence scores for the classes
-            float maxV = float.NegativeInfinity;
-            int maxIndex = -1;
-            for (int i = 5; i < numCols; i++)
-            {
-                float conf = outTensor[r, i] * sc;
-                if (conf > maxV)
-                {
-                    maxV = conf;
-                    maxIndex = i;
-                }
-            }
+			// Retrieve the confidence scores for the classes
+			float maxV = float.NegativeInfinity;
+			int maxIndex = -1;
+			for (int i = 5; i < numCols; i++)
+			{
+				float conf = outTensor[r, i] * sc;
+				if (conf > maxV)
+				{
+					maxV = conf;
+					maxIndex = i;
+				}
+			}
 
-            scores.Add(maxV);
-            boxes.Add(new Rect(cx - w / 2, cy - h / 2, w, h));
-            indices.Add(r);
-        }
-
-
-        Debug.Log("TODO - DetectionThreshold / NmsThreshold / ConfidenceThreshold stuff");
-
-        // Use the boxes, scores, and indices as needed
-        return boxes;
-    }
+			scores.Add(maxV);
+			boxes.Add(new Rect(cx - w / 2, cy - h / 2, w, h));
+			indices.Add(r);
+		}
 
 
+		Debug.Log("TODO - DetectionThreshold / NmsThreshold / ConfidenceThreshold stuff");
+
+		// Use the boxes, scores, and indices as needed
+		return boxes;
+	}
 
 
-    public static List<FaceONNX.FaceDetectionResult> ReadTensor(
-        float DetectionThreshold,
-        float NmsThreshold,
-        float ConfidenceThreshold,
-
-        (int, int) inputSize,
-
-        float[] data)
-    {
-
-        using StreamWriter writer = new StreamWriter("face_onnx_way.csv");
-
-        // https://github.com/FaceONNX/FaceONNX/blob/main/netstandard/FaceONNX/face/classes/FaceDetector.cs#L108
 
 
-        var Labels = new string[] { "face" };
-        var size = new Size(inputSize.Item1, inputSize.Item2);
-        var width = size.Width; //  image[0].GetLength(1);
-        var height = size.Height; //  image[0].GetLength(0);
+	public static List<FaceONNX.FaceDetectionResult> ReadTensor(
+		float DetectionThreshold,
+		float NmsThreshold,
+		float ConfidenceThreshold,
 
-        // yolo params
-        var yoloSquare = 15;
-        var classes = Labels.Length;
-        var count = classes + yoloSquare;
+		(int, int) inputSize,
 
-        // post-processing
-        var vector = data; // results[0].AsTensor<float>().ToArray();
-        var length = vector.Length / count;
-        var predictions = new float[length][];
+		float[] data)
+	{
 
-        for (int i = 0; i < length; i++)
-        {
-            var prediction = new float[count];
+		using StreamWriter writer = new StreamWriter("face_onnx_way.csv");
 
-            for (int j = 0; j < count; j++)
-                prediction[j] = vector[i * count + j];
+		// https://github.com/FaceONNX/FaceONNX/blob/main/netstandard/FaceONNX/face/classes/FaceDetector.cs#L108
 
-            predictions[i] = prediction;
-        }
 
-        var list = new List<float[]>();
+		var Labels = new string[] { "face" };
+		var size = new Size(inputSize.Item1, inputSize.Item2);
+		var width = size.Width; //  image[0].GetLength(1);
+		var height = size.Height; //  image[0].GetLength(0);
 
-        // seivining results
-        for (int i = 0; i < length; i++)
-        {
-            var prediction = predictions[i];
+		// yolo params
+		var yoloSquare = 15;
+		var classes = Labels.Length;
+		var count = classes + yoloSquare;
 
-            if (prediction[4] > DetectionThreshold)
-            {
-                var a = prediction[0];
-                var b = prediction[1];
-                var c = prediction[2];
-                var d = prediction[3];
+		// post-processing
+		var vector = data; // results[0].AsTensor<float>().ToArray();
+		var length = vector.Length / count;
+		var predictions = new float[length][];
 
-                prediction[0] = a - c / 2;
-                prediction[1] = b - d / 2;
-                prediction[2] = a + c / 2;
-                prediction[3] = b + d / 2;
+		for (int i = 0; i < length; i++)
+		{
+			var prediction = new float[count];
 
-                //for (int j = yoloSquare; j < prediction.Length; j++)
-                //{
-                //	prediction[j] *= prediction[4];
-                //}
+			for (int j = 0; j < count; j++)
+				prediction[j] = vector[i * count + j];
 
-                list.Add(prediction);
-            }
-        }
+			predictions[i] = prediction;
+		}
 
-        // non-max suppression
-        list = FaceONNX.NonMaxSuppressionExensions.AgnosticNMSFiltration(list, NmsThreshold);
+		var list = new List<float[]>();
 
-        // perform
-        predictions = list.ToArray();
-        length = predictions.Length;
+		// seivining results
+		for (int i = 0; i < length; i++)
+		{
+			var prediction = predictions[i];
 
-        // backward transform
-        var k0 = (float)size.Width / width;
-        var k1 = (float)size.Height / height;
-        float gain = Mathf.Min(k0, k1);
-        float p0 = (size.Width - width * gain) / 2;
-        float p1 = (size.Height - height * gain) / 2;
+			if (prediction[4] > DetectionThreshold)
+			{
+				var a = prediction[0];
+				var b = prediction[1];
+				var c = prediction[2];
+				var d = prediction[3];
 
-        // collect results
-        var detectionResults = new List<FaceONNX.FaceDetectionResult>();
+				prediction[0] = a - c / 2;
+				prediction[1] = b - d / 2;
+				prediction[2] = a + c / 2;
+				prediction[3] = b + d / 2;
 
-        for (int i = 0; i < length; i++)
-        {
-            var prediction = predictions[i];
-            var labels = new float[classes];
+				//for (int j = yoloSquare; j < prediction.Length; j++)
+				//{
+				//	prediction[j] *= prediction[4];
+				//}
 
-            for (int j = 0; j < classes; j++)
-            {
-                labels[j] = prediction[j + yoloSquare];
-            }
+				list.Add(prediction);
+			}
+		}
 
-            var max = UMapx.Core.Matrice.Max(labels, out int argmax);
+		// non-max suppression
+		list = FaceONNX.NonMaxSuppressionExensions.AgnosticNMSFiltration(list, NmsThreshold);
 
-            if (max > ConfidenceThreshold)
-            {
-                var rectangle = Rectangle.FromLTRB(
-                    (int)((prediction[0] - p0) / gain),
-                    (int)((prediction[1] - p1) / gain),
-                    (int)((prediction[2] - p0) / gain),
-                    (int)((prediction[3] - p1) / gain));
+		// perform
+		predictions = list.ToArray();
+		length = predictions.Length;
 
-                var points = new Point[5];
+		// backward transform
+		var k0 = (float)size.Width / width;
+		var k1 = (float)size.Height / height;
+		float gain = Mathf.Min(k0, k1);
+		float p0 = (size.Width - width * gain) / 2;
+		float p1 = (size.Height - height * gain) / 2;
 
-                for (int j = 0; j < 5; j++)
-                {
-                    points[j] = new Point
-                    {
-                        X = (int)((prediction[5 + 2 * j + 0] - p0) / gain),
-                        Y = (int)((prediction[5 + 2 * j + 1] - p1) / gain)
-                    };
-                }
+		// collect results
+		var detectionResults = new List<FaceONNX.FaceDetectionResult>();
 
-                var landmarks = new FaceONNX.Face5Landmarks(points);
+		for (int i = 0; i < length; i++)
+		{
+			var prediction = predictions[i];
+			var labels = new float[classes];
 
-                //writer.Write($"{rectangle.Left}, {rectangle.Top}, {rectangle.Right}, {rectangle.Top},\n");
+			for (int j = 0; j < classes; j++)
+			{
+				labels[j] = prediction[j + yoloSquare];
+			}
 
-                detectionResults.Add(new FaceONNX.FaceDetectionResult
-                {
-                    Rectangle = rectangle,
-                    Id = argmax,
-                    Score = max,
-                    Points = landmarks
-                });
-            }
-        }
+			var max = UMapx.Core.Matrice.Max(labels, out int argmax);
 
-        return detectionResults;
+			if (max > ConfidenceThreshold)
+			{
+				var rectangle = Rectangle.FromLTRB(
+					(int)((prediction[0] - p0) / gain),
+					(int)((prediction[1] - p1) / gain),
+					(int)((prediction[2] - p0) / gain),
+					(int)((prediction[3] - p1) / gain));
 
-    }
+				var points = new Point[5];
+
+				for (int j = 0; j < 5; j++)
+				{
+					points[j] = new Point
+					{
+						X = (int)((prediction[5 + 2 * j + 0] - p0) / gain),
+						Y = (int)((prediction[5 + 2 * j + 1] - p1) / gain)
+					};
+				}
+
+				var landmarks = new FaceONNX.Face5Landmarks(points);
+
+				//writer.Write($"{rectangle.Left}, {rectangle.Top}, {rectangle.Right}, {rectangle.Top},\n");
+
+				detectionResults.Add(new FaceONNX.FaceDetectionResult
+				{
+					Rectangle = rectangle,
+					Id = argmax,
+					Score = max,
+					Points = landmarks
+				});
+			}
+		}
+
+		return detectionResults;
+
+	}
 }
