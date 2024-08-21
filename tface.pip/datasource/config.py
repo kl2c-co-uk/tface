d245858f0cc5620a48f15f9aeff7f2281c535c48
Peter LaValle <peter.lavalle@gmail.com>
2024-08-21 11:28:15 +0100

    ready
    --HG--
    branch : cartoon-training

--- tface.pip/datasource/config.py
+++ tface.pip/datasource/config.py
@@ -10,16 +10,19 @@ from datasource import todo
 # python yolo5.py --weights "C:/Users/peter/Desktop/tface-train/tface.pip/target/yolov5/runs/train/exp12/weights/best.pt" && shutdown /s /t 180
 # 20 epoch
 # 128 input size for better accuracy
-# 128 batch size to avoid crashes
+# 72 batch size to avoid crashes - based on napkin-math; this should stay under 2G
 
-INPUT_SIZE = 96 # very small to make training fast
-BATCH_SIZE = 192
+INPUT_SIZE = 128 #
+BATCH_SIZE = 72 # 
 
 # 96 and 16 = 0.24G
 # 96 and 50 = 0.42G
 # 96 and 128 = 0.814G
 # 96 and 256 =? =2.52G
-# 96 and 192 =1.2G     <-- seems to be best match
+# 96 and 192    =1.2G     <-- seems to be best match
+#               =1.98G at the end; later epochs take more RAM and I don't know why
+
+# 128,72 =
 
 # 640 (or 320?) was the default
 # RTFM; -1 batch size handles it automatically
